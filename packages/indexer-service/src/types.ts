7648c8f7e4c5113b1fdcefa98c1293152b48be2e
Jannis Pohlmann <jannis@thegraph.com>
2020-05-04 15:52:51 +0200

    indexer-service: Add component interfaces

---
+++ packages/indexer-service/src/types.ts
@@ -0,0 +1,57 @@
+import { EventEmitter } from 'events'
+import { BigNumber } from 'ethers/utils'
+
+export interface QueryResult {
+  requestCid: string
+  responseCid: string
+  attestation: string
+  graphQLResponse: string
+}
+
+export interface PaidQueryResponse {
+  result: QueryResult
+  status: number
+}
+
+export interface PaidQuery {
+  subgraphId: string
+  paymentId: string
+  query: string
+}
+
+export interface FreeQuery {
+  subgraphId: string
+  query: string
+}
+
+export interface FreeQueryResponse {
+  subgraphId: string
+  status: number
+  data: any
+}
+
+export interface PaidQueryProcessor {
+  addPaidQuery(query: PaidQuery): Promise<PaidQueryResponse>
+  addPayment(payment: ConditionalPayment): Promise<void>
+}
+
+export interface FreeQueryProcessor {
+  addFreeQuery(query: FreeQuery): Promise<FreeQueryResponse>
+}
+
+export interface ConditionalPayment {
+  paymentId: string
+  amount: BigNumber
+  sender: string
+  signer: string
+}
+
+export interface ConditionalPaymentUnlockInfo {
+  paymentId: string
+  amount: BigNumber
+  attestation: string
+}
+
+export interface PaymentManager extends EventEmitter {
+  unlockPayment(info: ConditionalPaymentUnlockInfo): Promise<void>
+}
