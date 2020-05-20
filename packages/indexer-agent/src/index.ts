61f687f0e4b3174369e0655d633c832c271576e7
Ford Nickels <ford@thegraph.com>
2020-05-20 13:49:33 -0700

    indexer-agent: Only log yargs.help on yarg input errors

--- packages/indexer-agent/src/index.ts
+++ packages/indexer-agent/src/index.ts
@@ -4,5 +4,17 @@ yargs
   .scriptName('indexer-agent')
   .env('INDEXER_AGENT')
   .command(require('./commands/start').default)
+  .fail(function(msg, err, yargs) {
+    if (err) {
+      console.error(err)
+    } else {
+      console.error(msg)
+      console.error(`
+Usage help...
+`)
+      console.error(yargs.help())
+    }
+    process.exit(1)
+  })
   .demandCommand(1, 'Choose a command from the above list')
   .help().argv
