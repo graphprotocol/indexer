223669d48e78def3742887923c98ada1a4b191ff
Jannis Pohlmann <jannis@thegraph.com>
2020-07-01 17:12:34 +0200

    indexer-agent: Stake at least 100000 GRT
    This was hard-coded to 100 so far, which means after only a
    single deployment that is allocated on with 100 GRT, the indexer
    has run out of stake available to allocate and can't handle
    more than that one deployment. 100000 GRT is enough to allocate
    100 GRT each on up to 1000 subgraphs.

--- packages/indexer-agent/src/agent.ts
+++ packages/indexer-agent/src/agent.ts
@@ -64,7 +64,7 @@ export class Agent {
     )
     await this.indexer.connect()
     await this.network.register()
-    await this.network.ensureMinimumStake(100)
+    await this.network.ensureMinimumStake(100000)
     this.logger.info(`Indexer active and registered on network..`)
   }
 
