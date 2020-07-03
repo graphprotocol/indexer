e68f4f14dd54180496cc16fc61c3ea96e7a074c8
Jannis Pohlmann <jannis@thegraph.com>
2020-07-03 12:36:00 +0200

    indexer-agent, indexer-service: Add eslint and format/lint NPM scripts

---
+++ packages/indexer-agent/.eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  root: false,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+}
