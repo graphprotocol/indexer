6c7b4105012d5dde16d413de99fd4a13043dede3
Liam Horne <liam@lihorne.com>
2020-08-10 15:47:59 -0400

    indexer-service: WIP AllocationClient and PaymentManager

--- packages/indexer-service/src/allocation-client.ts
+++ packages/indexer-service/src/allocation-client.ts
@@ -1,53 +1,206 @@
+import { Wallet as ServerWallet } from '@statechannels/server-wallet'
+import { Message as WireMessage } from '@statechannels/client-api-schema'
+import { Message as PushMessage } from '@statechannels/wallet-core'
 import { Logger, Attestation } from '@graphprotocol/common-ts'
 
-import { Wallet } from 'ethers'
+import { Wallet as EthersWallet } from 'ethers'
 
 import {
   AllocationPaymentClient as AllocationPaymentClientInterface,
   Allocation,
+  PaidQuery,
 } from './types'
 
 interface AllocationPaymentClientOptions {
-    allocation: Allocation
-    logger: Logger
-    client: null // May re-use these properties
-    signer: null // so keeping "null" for now
-    wallet: Wallet
+  allocation: Allocation
+  logger: Logger
+  client: null // May re-use these properties
+  signer: null // so keeping "null" for now
+  wallet: EthersWallet
+}
+
+export class AllocationPaymentClient implements AllocationPaymentClientInterface {
+  channelIds: Record<string, string> = {}
+  allocation: Allocation
+  wallet: EthersWallet
+
+  private logger: Logger
+  private client: null
+  private signer: null
+
+  private serverWallet: ServerWallet = new ServerWallet() //TODO: put unique pk in here?
+
+  constructor({
+    allocation,
+    logger,
+    client,
+    signer,
+    wallet,
+  }: AllocationPaymentClientOptions) {
+    this.allocation = allocation
+    this.wallet = wallet
+    this.logger = logger
+    this.client = client
+    this.signer = signer
   }
-  
-  export class AllocationPaymentClient implements AllocationPaymentClientInterface {
-    allocation: Allocation
-    wallet: Wallet
-  
-    private logger: Logger
-    private client: null
-    private signer: null
-  
-    constructor({
-      allocation,
-      logger,
-      client,
-      signer,
-      wallet,
-    }: AllocationPaymentClientOptions) {
-      this.allocation = allocation
-      this.wallet = wallet
-  
-      this.logger = logger
-      this.client = client
-      this.signer = signer
+
+  async handleMessage({ data, sender }: WireMessage): Promise<WireMessage | undefined> {
+    this.logger.info(`AllocationPaymentClient received message from ${sender}`)
+
+    const {
+      channelResults: [channel],
+      outbox,
+    } = await this.serverWallet.pushMessage(data as PushMessage)
+
+    if (!channel) throw Error('Received a new state that did nothing')
+
+    /**
+     * Initial request to create a channel is received. In this case, join
+     * the channel and — we assume it is unfunded here — auto-advance to
+     * the running stage. Two outbound messages (turnNum 0 and 3) to be sent.
+     */
+    if (channel.status === 'proposed' && outbox.length === 0) {
+      this.channelIds[sender] = channel.channelId
+
+      const {
+        outbox: [
+          { params: outboundJoinedChannelState },
+          { params: outboundFundedChannelState },
+        ],
+      } = await this.serverWallet.joinChannel(channel)
+
+      const me = await this.serverWallet.getParticipant()
+
+      if (!me) throw new Error('unreachable')
+
+      return {
+        sender: me.participantId,
+        recipient: sender,
+        data: {
+          signedStates: [
+            // eslint-disable-next-line
+            ((outboundJoinedChannelState as WireMessage).data as PushMessage)
+              .signedStates![0],
+
+            // eslint-disable-next-line
+            ((outboundFundedChannelState as WireMessage).data as PushMessage)
+              .signedStates![0],
+          ],
+        },
+      }
     }
-  
-    async unlockPayment(
-      // eslint-disable-next-line
-      attestation: Attestation,
-    ): Promise<string> {
-      // TODO: (Liam) Update the state channel and return the new state to send back to the consumer
-      throw new Error('Unimplemented - unlockPayment')
+
+    /**
+     * This is an expected response from the counterparty upon seeing 0 and 3,
+     * they will countersign 3 and send it back. Now, we don't need to reply.
+     */
+    if (channel.status === 'closed' && outbox.length === 1) {
+      const [{ params: outboundClosedChannelState }] = outbox
+
+      const me = await this.serverWallet.getParticipant()
+
+      if (!me) throw new Error('unreachable')
+
+      return {
+        sender: me.participantId,
+        recipient: sender,
+        data: {
+          signedStates: [
+            // eslint-disable-next-line
+            ((outboundClosedChannelState as WireMessage).data as PushMessage)
+              .signedStates![0],
+          ],
+        },
+      }
     }
-  
-    async settle(): Promise<void> {
-      // tell state channels wallet to closeChannel
-    }
+
+    throw new Error('unreachable')
   }
-  
+
+  // eslint-disable-next-line
+  async validatePayment(query: PaidQuery): Promise<void> {
+    const { subgraphDeploymentID, stateChannelMessage, requestCID } = query
+
+    //
+    // Decode as Message (client-api-schema?)
+    //
+    //    throw if not valid message
+    //    take decoded message
+    //
+
+    await this.serverWallet.pushMessage(stateChannelMessage.data)
+
+    // Push decoded message into the wallet
+    //
+    //    raise exception if wallet raises one
+    //    take ChannelUpdated / ChannelResult
+    //
+
+    // Verify channel update matches other parameters
+    //
+    //    auto-decline query
+    // throw {
+    //   ...Error('Provided state does not align with query, denied by wallet.'),
+    //   envelopedResponse: await allocationClient.declineQuery(),
+    // }
+    //
+    // There are two types of failures here:
+    //
+    // - If the request is just nonsensical, the pushMessage will return an error because
+    //   the transition will be invalid. In that case, verifyQuery should return false or
+    //   something and then HTTP response should return a 40x or some equivalent "bad query"
+    //
+    // - If the request is sensical but does not match the signed state. In this case the
+    //   query should still return a 40x or 50x but include a QueryDeclined signed state
+    //   (see below) -- this is not implemented yet.
+
+    return
+  }
+
+  // eslint-disable-next-line
+  async provideAttestation(
+    query: PaidQuery,
+    attestation: Attestation,
+  ): Promise<WireMessage> {
+    const channelId = this.channelIds[query.stateChannelMessage.sender]
+
+    const {
+      channelResult: { appData, allocations },
+    } = await this.serverWallet.getState({ channelId })
+
+    const {
+      outbox: [{ params: outboundMsg }],
+    } = await this.serverWallet.updateChannel({
+      channelId,
+      appData,
+      allocations,
+    })
+
+    return outboundMsg as WireMessage
+  }
+
+  async declineQuery(query: PaidQuery): Promise<WireMessage> {
+    const channelId = this.channelIds[query.stateChannelMessage.sender]
+
+    const {
+      channelResult: { appData, allocations },
+    } = await this.serverWallet.getState({ channelId })
+
+    const {
+      outbox: [{ params: outboundMsg }],
+    } = await this.serverWallet.updateChannel({
+      channelId,
+      appData,
+      allocations,
+    })
+
+    return outboundMsg as WireMessage
+  }
+
+  async settle(): Promise<void> {
+    // We expect the gateway to close the channel, so here we can do some logic like
+    // waiting for 5 minutes for the close channel state to be signed and if we don't see
+    // it, then go to chain to forceMove the closure. Otherwise, if we have it
+    // then go to chain with a withdrawal transaction to get the money instantly.
+  }
+}
