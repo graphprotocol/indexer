59e6fc7eba1465eecffdd98ee8effa16d69469a2
geoknee <georgeknee@googlemail.com>
2020-08-13 17:39:27 +0100

    indexer-service: Fixup eslint config and run yarn

--- packages/indexer-agent/.eslintrc.js
+++ packages/indexer-agent/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  root: false,
+  root: true,
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
