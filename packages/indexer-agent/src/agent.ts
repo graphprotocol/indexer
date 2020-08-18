ddecd28738d867b86f4181ba6ecb97cc5487d5d6
Jannis Pohlmann <jannis@thegraph.com>
2020-08-18 02:40:58 +0200

    indexer-agent: Don't wait 2s after ensuring subgraph deployments

--- packages/indexer-agent/src/agent.ts
+++ packages/indexer-agent/src/agent.ts
@@ -281,10 +281,6 @@ class Agent {
         // Note: we're not waiting here, as sometimes indexing a subgrah
         // will block if the IPFS files cannot be retrieved
         this.indexer.ensure(name, deployment)
-
-        // Instead of blocking, we're simply sleeping for a bit;
-        // that way we don't do too much at the same time
-        await delay(2000)
       })
     }
 
