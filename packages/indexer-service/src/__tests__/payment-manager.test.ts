7c73b63276369628d8fe17deaee8289b2659be78
Mike Kerzhner <mikerzhner@gmail.com>
2020-08-21 11:51:21 -0700

    Remove nesting from tests

--- packages/indexer-service/src/__tests__/payment-manager.test.ts
+++ packages/indexer-service/src/__tests__/payment-manager.test.ts
@@ -35,29 +35,20 @@ import { toJS, StateType } from '@statechannels/graph'
 
 const logger = createLogger({ name: 'server.test.ts' })
 
+let paymentManager: PaymentManager
+beforeAll(async () => {
+  paymentManager = new PaymentManager({
+    wallet: Wallet.createRandom(),
+    logger: logger.child({ component: 'PaymentManager' }),
+    metrics: createMetrics(),
+  })
+})
+
+afterAll(async () => {
+  await serverWalletKnex.destroy()
+})
+
 describe('PaymentManager', () => {
-  let paymentManager: PaymentManager
-
-  beforeAll(async () => {
-    logger.info(`Truncating ${process.env.SERVER_DB_NAME}; Seeding new SigningWallet`)
-    await seedAlicesSigningWallet(serverWalletKnex)
-
-    paymentManager = new PaymentManager({
-      wallet: Wallet.createRandom(),
-      logger: logger.child({ component: 'PaymentManager' }),
-      metrics: createMetrics(),
-    })
-  })
-
-  afterAll(async () => {
-    await serverWalletKnex.destroy()
-  })
-
-  afterEach(async () => {
-    await serverWalletKnex('funding').truncate()
-    await serverWalletKnex('channels').truncate()
-  })
-
   it('is defined', async () => {
     expect(paymentManager).toBeDefined()
   })
@@ -69,88 +60,93 @@ describe('PaymentManager', () => {
     expect(client).toBeDefined()
     expect(client!.allocation).toBe(testAlloc)
   })
+})
 
-  describe('AllocationClient', () => {
-    let allocationClient: AllocationPaymentClient
+describe.only('AllocationClient', () => {
+  let allocationClient: AllocationPaymentClient
 
-    beforeAll(() => {
-      const testAlloc = mockAllocation()
-      paymentManager.createAllocationPaymentClients([testAlloc])
-      allocationClient = paymentManager.getAllocationPaymentClient(constants.AddressZero)!
-    })
+  beforeAll(() => {
+    const testAlloc = mockAllocation()
+    paymentManager.createAllocationPaymentClients([testAlloc])
+    allocationClient = paymentManager.getAllocationPaymentClient(constants.AddressZero)!
+  })
 
-    it('can call joinChannel and auto-sign funding state', async () => {
-      const outbound = await allocationClient.handleMessage(mockCreatedChannelMessage())
+  beforeEach(async () => {
+    logger.info(`Truncating ${process.env.SERVER_DB_NAME}; Seeding new SigningWallet`)
+    await seedAlicesSigningWallet(serverWalletKnex)
+  })
 
-      const {
-        data: {
-          signedStates: [outboundOne, outboundTwo],
-        },
-      } = outbound as { data: { signedStates: SignedState[] } }
+  it('can call joinChannel and auto-sign funding state', async () => {
+    const outbound = await allocationClient.handleMessage(mockCreatedChannelMessage())
 
-      expect(outboundOne).toMatchObject({ turnNum: 0 })
-      expect(outboundTwo).toMatchObject({ turnNum: 3 })
-    })
+    const {
+      data: {
+        signedStates: [outboundOne, outboundTwo],
+      },
+    } = outbound as { data: { signedStates: SignedState[] } }
 
-    it('can validate a payment', async () => {
-      await expect(allocationClient.validatePayment(mockQuery())).resolves.not.toThrow()
-    })
+    expect(outboundOne).toMatchObject({ turnNum: 0 })
+    expect(outboundTwo).toMatchObject({ turnNum: 3 })
+  })
 
-    it('can provide attestation response', async () => {
-      await allocationClient.handleMessage(mockCreatedChannelMessage())
-      await allocationClient.validatePayment(mockQuery())
+  it('can validate a payment', async () => {
+    await expect(allocationClient.validatePayment(mockQuery())).resolves.not.toThrow()
+  })
 
-      const attestationMessage = await allocationClient.provideAttestation(
-        mockChannelId,
-        mockQuery(),
-        mockAttestation(),
-      )
-      const {
-        data: {
-          signedStates: [nextState],
-        },
-      } = attestationMessage as { data: { signedStates: SignedState[] } }
-      const appData = toJS(nextState.appData)
+  it('can provide attestation response', async () => {
+    await allocationClient.handleMessage(mockCreatedChannelMessage())
+    await allocationClient.validatePayment(mockQuery())
 
-      expect(appData.constants).toEqual(mockAppData().constants)
-      expect(appData.variable.responseCID).toEqual(mockAttestation().responseCID)
-      expect(appData.variable.stateType).toEqual(StateType.AttestationProvided)
-      expect((nextState.outcome as SimpleAllocation).allocationItems).toEqual([
-        { amount: BN.from(99), destination: makeDestination(constants.AddressZero) },
-        { amount: BN.from(1), destination: makeDestination(constants.AddressZero) },
-      ])
-    })
+    const attestationMessage = await allocationClient.provideAttestation(
+      mockChannelId,
+      mockQuery(),
+      mockAttestation(),
+    )
+    const {
+      data: {
+        signedStates: [nextState],
+      },
+    } = attestationMessage as { data: { signedStates: SignedState[] } }
+    const appData = toJS(nextState.appData)
 
-    it('can deny a query', async () => {
-      await allocationClient.handleMessage(mockCreatedChannelMessage())
-      await allocationClient.validatePayment(mockQuery())
-      const outbound = await allocationClient.declineQuery(mockChannelId, mockQuery())
+    expect(appData.constants).toEqual(mockAppData().constants)
+    expect(appData.variable.responseCID).toEqual(mockAttestation().responseCID)
+    expect(appData.variable.stateType).toEqual(StateType.AttestationProvided)
+    expect((nextState.outcome as SimpleAllocation).allocationItems).toEqual([
+      { amount: BN.from(99), destination: makeDestination(constants.AddressZero) },
+      { amount: BN.from(1), destination: makeDestination(constants.AddressZero) },
+    ])
+  })
 
-      const {
-        data: {
-          signedStates: [nextState],
-        },
-      } = outbound as { data: { signedStates: SignedState[] } }
+  it('can deny a query', async () => {
+    await allocationClient.handleMessage(mockCreatedChannelMessage())
+    await allocationClient.validatePayment(mockQuery())
+    const outbound = await allocationClient.declineQuery(mockChannelId, mockQuery())
 
-      const appData = toJS(nextState.appData)
-      expect(appData.constants).toEqual(mockAppData().constants)
-      expect(appData.variable.stateType).toEqual(StateType.QueryDeclined)
+    const {
+      data: {
+        signedStates: [nextState],
+      },
+    } = outbound as { data: { signedStates: SignedState[] } }
 
-      expect(nextState).toMatchObject({ turnNum: 5 })
-    })
+    const appData = toJS(nextState.appData)
+    expect(appData.constants).toEqual(mockAppData().constants)
+    expect(appData.variable.stateType).toEqual(StateType.QueryDeclined)
 
-    it('can accept a channel closure', async () => {
-      await allocationClient.handleMessage(mockCreatedChannelMessage())
-      await allocationClient.handleMessage(mockPostFundMessage())
-      const outbound = await allocationClient.handleMessage(mockCloseChannelMessage())
+    expect(nextState).toMatchObject({ turnNum: 5 })
+  })
 
-      const {
-        data: {
-          signedStates: [outboundState],
-        },
-      } = outbound as { data: { signedStates: SignedState[] } }
+  it.only('can accept a channel closure', async () => {
+    await allocationClient.handleMessage(mockCreatedChannelMessage())
+    await allocationClient.handleMessage(mockPostFundMessage())
+    const outbound = await allocationClient.handleMessage(mockCloseChannelMessage())
 
-      expect(outboundState).toMatchObject({ turnNum: 4, isFinal: true })
-    })
+    const {
+      data: {
+        signedStates: [outboundState],
+      },
+    } = outbound as { data: { signedStates: SignedState[] } }
+
+    expect(outboundState).toMatchObject({ turnNum: 4, isFinal: true })
   })
 })
