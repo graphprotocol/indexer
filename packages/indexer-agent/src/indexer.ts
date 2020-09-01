504ea31ac5244af90aa533b77aca4c6d17b4f874
Jannis Pohlmann <jannis@thegraph.com>
2020-09-01 10:02:56 +0200

    indexer-agent: Rename indexerRules to indexingRules

--- packages/indexer-agent/src/indexer.ts
+++ packages/indexer-agent/src/indexer.ts
@@ -86,7 +86,7 @@ export class Indexer {
     }
   }
 
-  async indexerRules(merged: boolean): Promise<IndexingRuleAttributes[]> {
+  async indexingRules(merged: boolean): Promise<IndexingRuleAttributes[]> {
     try {
       const result = await this.indexerManagement
         .query(
