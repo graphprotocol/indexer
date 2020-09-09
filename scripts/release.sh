8a702ddc6046787ab25c7720814aa4f6c1796346
Jannis Pohlmann <jannis@thegraph.com>
2020-09-09 09:56:06 +0200

    NPM: Tweak release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,17 +3,22 @@
 set -e
 set -x
 
-# Install dependencies fresh and rebuild packages
-rm -rf node_modules packages/*/node_modules
-yarn
-
 # Publish to NPM
+rm -rf node_modules packages/*/node_modules
+yarn --registry https://registry.npmjs.org/
 lerna publish
 
-# Publish to the private NPM registry
-git clone git@github.com:graphprotocol/indexer.git /tmp/indexer-publish
-cd /tmp/indexer-publish
-sed -i -e s@registry.npmjs.org@testnet.thegraph.com/npm-registry@g yarn.lock
-sed -i -e s@registry.yarnpkg.com@testnet.thegraph.com/npm-registry@g yarn.lock
-rm yarn.lock-e
-npm publish --dry-run --registry https://testnet.thegraph.com/npm-registry/
+# Publish to testnet NPM registry
+git clone git@github.com:graphprotocol/indexer.git /tmp/indexer-release
+pushd /tmp/indexer-release
+(
+  rm yarn.lock
+  yarn --registry https://testnet.thegraph.com/npm-registry/
+  for package in packages/*; do
+    pushd $package
+    npm publish --registry https://testnet.thegraph.com/npm-registry/
+    popd
+  done
+)
+popd
+rm -rf /tmp/indexer-release
