a8c422e9255767809720df1c7018ddd1bf34fd19
Jannis Pohlmann <jannis@thegraph.com>
2020-10-28 15:12:23 +0100

    indexer-common: Preserve $DAI on updates, inject it into new models

--- packages/indexer-common/src/indexer-management/client.ts
+++ packages/indexer-common/src/indexer-management/client.ts
@@ -1,8 +1,15 @@
 import { buildSchema, print } from 'graphql'
 import gql from 'graphql-tag'
 import { executeExchange } from '@urql/exchange-execute'
-import { createClient, Client } from '@urql/core'
-import { Logger, NetworkContracts } from '@graphprotocol/common-ts'
+import { Client, ClientOptions } from '@urql/core'
+import {
+  equal,
+  Eventual,
+  Logger,
+  mutable,
+  NetworkContracts,
+  WritableEventual,
+} from '@graphprotocol/common-ts'
 
 import { IndexerManagementModels, IndexingRuleCreationAttributes } from './models'
 
@@ -10,6 +17,11 @@ import indexingRuleResolvers from './resolvers/indexing-rules'
 import statusResolvers from './resolvers/indexer-status'
 import costModelResolvers from './resolvers/cost-models'
 import { BigNumber } from 'ethers'
+import { Op, Sequelize } from 'sequelize'
+
+export interface IndexerManagementFeatures {
+  injectDai: boolean
+}
 
 export interface IndexerManagementResolverContext {
   models: IndexerManagementModels
@@ -17,6 +29,8 @@ export interface IndexerManagementResolverContext {
   contracts: NetworkContracts
   logger?: Logger
   defaults: IndexerManagementDefaults
+  features: IndexerManagementFeatures
+  dai: Eventual<string>
 }
 
 const SCHEMA_SDL = gql`
@@ -128,23 +142,66 @@ export interface IndexerManagementClientOptions {
   contracts: NetworkContracts
   logger?: Logger
   defaults: IndexerManagementDefaults
+  features: IndexerManagementFeatures
 }
 
-export type IndexerManagementClient = Client
+export class IndexerManagementClient extends Client {
+  private logger?: Logger
+  private models: IndexerManagementModels
+  private dai: WritableEventual<string>
 
-export const createIndexerManagementClient = async ({
-  models,
-  address,
-  contracts,
-  logger,
-  defaults,
-}: IndexerManagementClientOptions): Promise<Client> => {
+  constructor(
+    clientOptions: ClientOptions,
+    options: IndexerManagementClientOptions,
+    featureOptions: { dai: WritableEventual<string> },
+  ) {
+    super(clientOptions)
+
+    this.logger = options.logger
+    this.models = options.models
+    this.dai = featureOptions.dai
+  }
+
+  public async setDai(value: string): Promise<void> {
+    // Get current value
+    const oldValue = this.dai.valueReady ? await this.dai.value() : undefined
+
+    // Don't do anything if there is no change
+    if (equal(oldValue, value)) {
+      return
+    }
+
+    // Notify others of the new value
+    this.dai.push(value)
+
+    // Update DAI in all cost models
+    await this.models.CostModel.update(
+      {
+        // This merges DAI into the variables, overwriting existing values
+        variables: Sequelize.literal(
+          `variables || '${JSON.stringify({ DAI: value })}'::jsonb`,
+        ),
+      },
+      {
+        // This is just a non-obvious way to match all models
+        where: { deployment: { [Op.not]: null } },
+      },
+    )
+  }
+}
+
+export const createIndexerManagementClient = async (
+  options: IndexerManagementClientOptions,
+): Promise<IndexerManagementClient> => {
+  const { models, address, contracts, logger, defaults, features } = options
   const schema = buildSchema(print(SCHEMA_SDL))
   const resolvers = {
     ...indexingRuleResolvers,
     ...statusResolvers,
     ...costModelResolvers,
   }
+
+  const dai: WritableEventual<string> = mutable()
 
   const exchange = executeExchange({
     schema,
@@ -155,8 +212,12 @@ export const createIndexerManagementClient = async ({
       contracts,
       logger: logger ? logger.child({ component: 'IndexerManagementClient' }) : undefined,
       defaults,
+      features,
+      dai,
     },
   })
 
-  return createClient({ url: 'no-op', exchanges: [exchange] })
+  return new IndexerManagementClient({ url: 'no-op', exchanges: [exchange] }, options, {
+    dai,
+  })
 }
--- packages/indexer-common/src/indexer-management/resolvers/cost-models.ts
+++ packages/indexer-common/src/indexer-management/resolvers/cost-models.ts
@@ -1,8 +1,44 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/ban-types */
 
-import { CostModelCreationAttributes } from '../models'
+import { CostModelVariables, GraphQLCostModel, parseGraphQLCostModel } from '../models'
 import { IndexerManagementResolverContext } from '../client'
+import { disableExperimentalFragmentVariables } from 'graphql-tag'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const getVariable = (vars: CostModelVariables | null, name: string): any | undefined => {
+  if (vars === null) {
+    return undefined
+  } else {
+    try {
+      if (Object.prototype.hasOwnProperty.call(vars, name)) {
+        return vars[name]
+      } else {
+        return undefined
+      }
+    } catch (e) {
+      return undefined
+    }
+  }
+}
+
+const setVariable = (
+  vars: CostModelVariables | null,
+  name: string,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  value: any,
+): CostModelVariables => {
+  if (vars === null) {
+    return { name: value }
+  } else {
+    try {
+      vars[name] = value
+      return vars
+    } catch (e) {
+      return vars
+    }
+  }
+}
 
 export default {
   costModel: async (
@@ -28,18 +64,36 @@ export default {
   },
 
   setCostModel: async (
-    { costModel }: { deployment: string; costModel: CostModelCreationAttributes },
-    { models }: IndexerManagementResolverContext,
+    { costModel }: { deployment: string; costModel: GraphQLCostModel },
+    { models, features, dai }: IndexerManagementResolverContext,
   ): Promise<object> => {
+    const update = parseGraphQLCostModel(costModel)
+
     const [model] = await models.CostModel.findOrBuild({
-      where: { deployment: costModel.deployment },
+      where: { deployment: update.deployment },
     })
     model.deployment = costModel.deployment || model.deployment
     model.model =
       costModel.model !== null && costModel.model !== undefined
         ? costModel.model
         : model.model
-    model.variables = costModel.variables || model.variables
+
+    if (features.injectDai) {
+      const oldDai = getVariable(model.variables, 'DAI')
+      const newDai = getVariable(update.variables, 'DAI')
+
+      // Make sure the Dai variable is never dropped
+      // Inject the latest DAI value if available
+      if (dai.valueReady) {
+        update.variables = setVariable(update.variables, 'DAI', await dai.value())
+      } else if (newDai === undefined && oldDai !== undefined) {
+        // Otherwise preserve the old DAI value if there is one
+        update.variables = setVariable(update.variables, 'DAI', oldDai)
+      }
+    }
+
+    model.variables = update.variables || model.variables
+
     return (await model.save()).toGraphQL()
   },
 }
