4a33f432366929f16d2266b8e98f523c7f522ecd
Jannis Pohlmann <jannis@thegraph.com>
2020-11-04 21:49:59 +0100

    *: Update common-ts to 0.3.11

--- scripts/update-common-ts.sh
+++ scripts/update-common-ts.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+VERSION=$1
+
+if [[ -z "$VERSION" ]]; then
+    echo "Usage: $0 <version>"
+    exit 1
+fi
+
 for pkg in $(ls -1 packages); do
     pushd packages/$pkg
-    yarn add @graphprotocol/common-ts
+    yarn add @graphprotocol/common-ts@$VERSION
     popd
 done
