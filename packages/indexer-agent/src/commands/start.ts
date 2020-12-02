0ed3371c82602258b9131d1a9e979e26b11aea02
Ford Nickels <ford@thegraph.com>
2020-12-02 11:40:26 -0800

    agent,service: Add data label to Eth req metric, several minor updates

--- packages/indexer-agent/src/commands/start.ts
+++ packages/indexer-agent/src/commands/start.ts
@@ -38,7 +38,7 @@ export default {
         group: 'Ethereum',
       })
       .option('ethereum-polling-interval', {
-        description: 'Polling interval for the Ethereum provider',
+        description: 'Polling interval for the Ethereum provider (ms)',
         type: 'number',
         default: 4000,
         group: 'Ethereum',
@@ -284,7 +284,7 @@ export default {
         name: 'eth_provider_requests',
         help: 'Ethereum provider requests',
         registers: [metrics.registry],
-        labelNames: ['method'],
+        labelNames: ['method', 'data'],
       }),
     }
 
@@ -295,19 +295,20 @@ export default {
     })
     ethereum.pollingInterval = argv.ethereumPollingInterval
 
-    if (argv.logLevel == 'trace') {
-      ethereum.on('debug', info => {
-        if (info.action == 'response') {
-          ethProviderMetrics.requests.inc({ method: info.request.method })
+    ethereum.on('debug', info => {
+      if (info.action === 'response') {
+        ethProviderMetrics.requests.inc({
+          method: info.request.method,
+          data: info.request.params?.data || '',
+        })
 
-          logger.trace('Provider request:', {
-            method: info.request.method,
-            params: info.request.params,
-            response: info.response,
-          })
-        }
-      })
-    }
+        logger.trace('Ethereum request', {
+          method: info.request.method,
+          params: info.request.params,
+          response: info.response,
+        })
+      }
+    })
 
     logger.info(`Connected to Ethereum`, {
       pollingInterval: ethereum.pollingInterval,
