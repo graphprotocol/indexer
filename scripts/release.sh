0a314e1e9cf5398e010c1a1dee79aa3465ae7fe6
Jannis Pohlmann <jannis@thegraph.com>
2020-12-22 15:20:49 +0100

    scripts: Simplify release script (remove testnet registry)

--- scripts/release.sh
+++ scripts/release.sh
@@ -25,20 +25,3 @@ done
 rm -rf node_modules packages/*/node_modules
 yarn --registry https://registry.npmjs.org/
 lerna publish "$VERSION"
-
-read  -n 1 -p "[Press any key to continue releasing to the testnet registry] "
-
-# Publish to testnet NPM registry
-git clone git@github.com:graphprotocol/indexer.git /tmp/indexer-release
-pushd /tmp/indexer-release
-(
-  rm yarn.lock
-  yarn --registry https://testnet.thegraph.com/npm-registry/
-  for package in packages/*; do
-    pushd $package
-    npm publish --registry https://testnet.thegraph.com/npm-registry/
-    popd
-  done
-)
-popd
-rm -rf /tmp/indexer-release
