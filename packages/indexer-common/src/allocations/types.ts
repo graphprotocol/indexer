9f03c8eb76230181457a1891c921ead7d4f719fd
Ford Nickels <ford@thegraph.com>
2021-01-06 11:57:02 -0800

    indexer-common: Add POI disputes to indexer management API

--- packages/indexer-common/src/allocations/types.ts
+++ packages/indexer-common/src/allocations/types.ts
@@ -13,10 +13,14 @@ export interface SubgraphDeployment {
 export interface Allocation {
   id: Address
   subgraphDeployment: SubgraphDeployment
+  indexer: string
+  allocatedTokens: BigNumber
   createdAtEpoch: number
   createdAtBlockHash: string
   closedAtEpoch: number
-  allocatedTokens: BigNumber
+  closedAtEpochStartBlockHash: string | undefined
+  closedAtBlockHash: string
+  poi: string | undefined
 }
 
 export enum AllocationStatus {
@@ -35,8 +39,56 @@ export const parseGraphQLAllocation = (allocation: any): Allocation => ({
     stakedTokens: BigNumber.from(allocation.subgraphDeployment.stakedTokens),
     signalAmount: BigNumber.from(allocation.subgraphDeployment.signalAmount),
   },
+  indexer: allocation.indexer.id,
   allocatedTokens: BigNumber.from(allocation.allocatedTokens),
   createdAtBlockHash: allocation.createdAtBlockHash,
   createdAtEpoch: allocation.createdAtEpoch,
+  closedAtEpochStartBlockHash: allocation.closedAtEpochStartBlockHash,
   closedAtEpoch: allocation.closedAtEpoch,
+  closedAtBlockHash: allocation.closedAtBlockHash,
+  poi: allocation.poi,
+})
+
+export interface RewardsPool {
+  subgraphDeployment: SubgraphDeploymentID
+  allocationIndexer: string
+  allocationCreatedAtBlockHash: string
+  closedAtEpoch: number
+  closedAtEpochStartBlockHash: string | undefined
+  referencePOI: string | undefined
+}
+
+export const allocationRewardsPool = (allocation: Allocation): RewardsPool => ({
+  subgraphDeployment: allocation.subgraphDeployment.id,
+  allocationIndexer: allocation.indexer,
+  allocationCreatedAtBlockHash: allocation.createdAtBlockHash,
+  closedAtEpoch: allocation.closedAtEpoch,
+  closedAtEpochStartBlockHash: allocation.closedAtEpochStartBlockHash,
+  referencePOI: undefined,
+})
+
+export interface Epoch {
+  id: number
+  startBlock: number
+  startBlockHash: string | undefined
+  endBlock: number
+  signalledTokens: number
+  stakeDeposited: number
+  queryFeeRebates: number
+  totalRewards: number
+  totalIndexerRewards: number
+  totalDelegatorRewards: number
+}
+
+export const parseGraphQLEpochs = (epoch: any): Epoch => ({
+  id: epoch.id,
+  startBlock: epoch.startBlock,
+  startBlockHash: undefined,
+  endBlock: epoch.endBlock,
+  signalledTokens: epoch.signalledTokens,
+  stakeDeposited: epoch.stakeDeposited,
+  queryFeeRebates: epoch.queryFeeRebates,
+  totalRewards: epoch.totalRewards,
+  totalIndexerRewards: epoch.totalIndexerRewards,
+  totalDelegatorRewards: epoch.totalDelegatorRewards,
 })
