9659756c5ac3a62c74daad15ffd778fcf08ffa4f
Jannis Pohlmann <jannis@edgeandnode.com>
2021-03-11 21:43:49 +0100

    indexer-agent: Add db migration to remove old state channel tables

---
+++ packages/indexer-agent/src/migrations/02-remove-state-channels.ts
@@ -0,0 +1,48 @@
+import { Logger } from '@graphprotocol/common-ts'
+import { QueryInterface } from 'sequelize'
+
+interface MigrationContext {
+  queryInterface: QueryInterface
+  logger: Logger
+}
+
+interface Context {
+  context: MigrationContext
+}
+
+export async function up({ context }: Context): Promise<void> {
+  const { queryInterface, logger } = context
+
+  logger.info(`Remove old state channels from the database`)
+
+  const tables = [
+    'app_bytecode',
+    'channels',
+    'funding',
+    'knex_migrations',
+    'knex_migrations_lock',
+    'ledger_requests',
+    'nonces',
+    'objectives',
+    'objectives_channels',
+    'signing_wallets',
+  ]
+
+  for (const table of tables) {
+    try {
+      await queryInterface.dropTable(table, { cascade: true })
+    } catch (err) {
+      logger.warn(
+        'Failed to remove old state channels table from the database',
+        {
+          err,
+          table,
+        },
+      )
+    }
+  }
+}
+
+export async function down(): Promise<void> {
+  // Nothing to do here
+}
