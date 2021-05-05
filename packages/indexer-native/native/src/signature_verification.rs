224ffc9d2fcb76279f64c37ca5daa04a84dd39bf
Zac Burns <That3Percent@gmail.com>
2021-05-05 12:07:16 -0500

    indexer-native: (Performance) Use verify instead of recover for signatures when possible

--- packages/indexer-native/native/src/signature_verification.rs
+++ packages/indexer-native/native/src/signature_verification.rs
@@ -22,18 +22,22 @@ impl SignatureVerifier {
         signature: &RecoverableSignature,
     ) -> Result<bool, &'static str> {
         let message = Message::from_slice(&keccak(&message).to_fixed_bytes()).unwrap();
-        let recovered_signer = SECP256K1
-            .recover(&message, &signature)
-            .map_err(|_| "Failed to recover signature")?;
 
         match self.signer.load().as_ref() {
             // If we already have the public key we can do the fast path.
-            Signer::PublicKey(signer) => Ok(signer == &recovered_signer),
+            Signer::PublicKey(signer) => Ok(SECP256K1
+                .verify(&message, &signature.to_standard(), signer)
+                .is_ok()),
             // If we don't have the public key, but have the address instead
             // we derive the address from the recovered key. If it's a match
             // then we can save the public key for the next time avoiding
-            // running keccak on every verification.
+            // running keccak on every verification and using the much faster
+            // verify method instead of the slow recover method.
             Signer::Address(addr) => {
+                let recovered_signer = SECP256K1
+                    .recover(&message, &signature)
+                    .map_err(|_| "Failed to recover signature")?;
+
                 let ser = recovered_signer.serialize_uncompressed();
                 debug_assert_eq!(ser[0], 0x04);
                 let pk_hash = keccak(&ser[1..]);
