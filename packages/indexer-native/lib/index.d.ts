4b3e44937325b40b8aa4c6b875486a82bef913c4
Jannis Pohlmann <jannis@edgeandnode.com>
2021-05-10 22:55:56 +0200

    indexer-native, indexer-service: Fix logging native signers

--- packages/indexer-native/lib/index.d.ts
+++ packages/indexer-native/lib/index.d.ts
@@ -2,12 +2,14 @@
     Verifies messages are signed by the same address.
     This API may seem strange compared to the relatively straightforward alternative:
        verify(address, message, signature): Promise<boolean>.
-    
+
     But, making the API this way allows for some optimizations:
        * The address is not repeatedly marshalled across the Rust/JS boundary
        * The PublicKey to Address conversion is cached, saving a keccak hash
 */
 export class NativeSignatureVerifier {
+  public readonly address: string;
+
   constructor(address: string);
   /*
         Verifies that the message was signed by the address this verifier
