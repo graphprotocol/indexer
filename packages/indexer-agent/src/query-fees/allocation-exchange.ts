4bca01af8c09cb8ef05ab3a091a94c17538f8504
Jannis Pohlmann <jannis@edgeandnode.com>
2021-05-12 16:02:11 +0200

    indexer-agent: Fix outdated `AllocationExchange` ABI
    We need to use the full ABI format here because human-readable
    APIs don't support structs like `AllocationVoucher` yet.

--- packages/indexer-agent/src/query-fees/allocation-exchange.ts
+++ packages/indexer-agent/src/query-fees/allocation-exchange.ts
@@ -2,8 +2,55 @@ import { Address } from '@graphprotocol/common-ts'
 import { Contract, providers, Signer } from 'ethers'
 
 const ABI = [
-  'function allocationsRedeemed(address allocationID) view returns (bool)',
-  'function redeem(address allocationID, uint256 amount, bytes signature)',
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    name: 'allocationsRedeemed',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        components: [
+          {
+            internalType: 'address',
+            name: 'allocationID',
+            type: 'address',
+          },
+          {
+            internalType: 'uint256',
+            name: 'amount',
+            type: 'uint256',
+          },
+          {
+            internalType: 'bytes',
+            name: 'signature',
+            type: 'bytes',
+          },
+        ],
+        internalType: 'struct AllocationExchange.AllocationVoucher',
+        name: '_voucher',
+        type: 'tuple',
+      },
+    ],
+    name: 'redeem',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
 ]
 
 export const bindAllocationExchangeContract = (
