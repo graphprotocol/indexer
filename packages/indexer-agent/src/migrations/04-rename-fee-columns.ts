412fd666218d4bda02097ff1f9f9bb2ea9be618f
Jannis Pohlmann <jannis@edgeandnode.com>
2021-05-25 16:55:29 +0200

    indexer-agent: Make fee columns migration more robust

--- packages/indexer-agent/src/migrations/04-rename-fee-columns.ts
+++ packages/indexer-agent/src/migrations/04-rename-fee-columns.ts
@@ -16,24 +16,34 @@ export async function up({ context }: Context): Promise<void> {
   logger.info(`Rename fee columns`)
 
   return await queryInterface.sequelize.transaction({}, async transaction => {
-    await queryInterface.renameColumn(
-      'allocation_receipts',
-      'paymentAmount',
-      'fees',
-      { transaction },
-    )
-    await queryInterface.renameColumn(
-      'allocation_summaries',
-      'queryFees',
-      'collectedFees',
-      { transaction },
-    )
-    await queryInterface.renameColumn(
-      'transfer_receipts',
-      'paymentAmount',
-      'fees',
-      { transaction },
-    )
+    const tables = await queryInterface.showAllTables()
+
+    if (tables.includes('allocation_receipts')) {
+      await queryInterface.renameColumn(
+        'allocation_receipts',
+        'paymentAmount',
+        'fees',
+        { transaction },
+      )
+    }
+
+    if (tables.includes('allocation_summaries')) {
+      await queryInterface.renameColumn(
+        'allocation_summaries',
+        'queryFees',
+        'collectedFees',
+        { transaction },
+      )
+    }
+
+    if (tables.includes('transfer_receipts')) {
+      await queryInterface.renameColumn(
+        'transfer_receipts',
+        'paymentAmount',
+        'fees',
+        { transaction },
+      )
+    }
   })
 }
 
@@ -43,23 +53,31 @@ export async function down({ context }: Context): Promise<void> {
   logger.info(`Revert renaming fee columns`)
 
   return await queryInterface.sequelize.transaction({}, async transaction => {
-    await queryInterface.renameColumn(
-      'allocation_receipts',
-      'fees',
-      'paymentAmount',
-      { transaction },
-    )
-    await queryInterface.renameColumn(
-      'allocation_summaries',
-      'collectedFees',
-      'queryFees',
-      { transaction },
-    )
-    await queryInterface.renameColumn(
-      'transfer_receipts',
-      'fees',
-      'paymentAmount',
-      { transaction },
-    )
+    const tables = await queryInterface.showAllTables()
+
+    if (tables.includes('allocation_receipts')) {
+      await queryInterface.renameColumn(
+        'allocation_receipts',
+        'fees',
+        'paymentAmount',
+        { transaction },
+      )
+    }
+    if (tables.includes('allocation_summaries')) {
+      await queryInterface.renameColumn(
+        'allocation_summaries',
+        'collectedFees',
+        'queryFees',
+        { transaction },
+      )
+    }
+    if (tables.includes('transfer_receipts')) {
+      await queryInterface.renameColumn(
+        'transfer_receipts',
+        'fees',
+        'paymentAmount',
+        { transaction },
+      )
+    }
   })
 }
