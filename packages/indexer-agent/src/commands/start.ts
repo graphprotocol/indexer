graphprotocol/indexer

commit 5febd002ec4668d9f1679f6d116f53fa836188a0

offchain subgraphs parameter fix

packages/indexer-agent/src/commands/start.ts
@@ -268,7 +268,7 @@ export default {
           arg.reduce(
             (acc: string[], value: string) => [...acc, ...value.split(',')],
             [],
-          ),
+          )).map((id: string) => id.trim()).filter((id: string) => id.length > 0),
       })
       .option('poi-disputable-epochs', {
         description:

commit 624e22203d9c5be0a9a130e80e9be52f62ad8182

indexer-agent: Fix syntax error in offchain-subgraphs option
Error introduced in 5febd002ec4668d9f1679f6d116f53fa836188a0

packages/indexer-agent/src/commands/start.ts
@@ -265,10 +265,13 @@ export default {
         array: true,
         default: [],
         coerce: arg =>
-          arg.reduce(
-            (acc: string[], value: string) => [...acc, ...value.split(',')],
-            [],
-          )).map((id: string) => id.trim()).filter((id: string) => id.length > 0),
+          arg
+            .reduce(
+              (acc: string[], value: string) => [...acc, ...value.split(',')],
+              [],
+            )
+            .map((id: string) => id.trim())
+            .filter((id: string) => id.length > 0),
       })
       .option('poi-disputable-epochs', {
         description: