f6acd5b69423661b5487bf294d1b33f99bbbb541
Ford <ford@thegraph.com>
2021-12-08 14:50:16 -0800

    indexer-agent: Formatting

--- packages/indexer-agent/src/agent.ts
+++ packages/indexer-agent/src/agent.ts
@@ -102,7 +102,8 @@ class Agent {
 
     const currentEpochStartBlock = currentEpoch.tryMap(
       async () => {
-        const startBlockNumber = await this.network.contracts.epochManager.currentEpochBlock()
+        const startBlockNumber =
+          await this.network.contracts.epochManager.currentEpochBlock()
         const startBlock = await this.network.ethereum.getBlock(
           startBlockNumber.toNumber(),
         )
@@ -443,13 +444,13 @@ class Agent {
           closedEpoch: allocation.closedAtEpoch,
           closedEpochReferenceProof: rewardsPool!.referencePOI,
           closedEpochStartBlockHash: allocation.closedAtEpochStartBlockHash!,
-          closedEpochStartBlockNumber: rewardsPool!
-            .closedAtEpochStartBlockNumber!,
+          closedEpochStartBlockNumber:
+            rewardsPool!.closedAtEpochStartBlockNumber!,
           previousEpochReferenceProof: rewardsPool!.referencePreviousPOI,
-          previousEpochStartBlockHash: rewardsPool!
-            .previousEpochStartBlockHash!,
-          previousEpochStartBlockNumber: rewardsPool!
-            .previousEpochStartBlockNumber!,
+          previousEpochStartBlockHash:
+            rewardsPool!.previousEpochStartBlockHash!,
+          previousEpochStartBlockNumber:
+            rewardsPool!.previousEpochStartBlockNumber!,
           status,
         } as POIDisputeAttributes
       },
@@ -739,9 +740,8 @@ class Agent {
       expiredAllocations,
       async (allocation: Allocation) => {
         try {
-          const onChainAllocation = await this.network.contracts.staking.getAllocation(
-            allocation.id,
-          )
+          const onChainAllocation =
+            await this.network.contracts.staking.getAllocation(allocation.id)
           return onChainAllocation.closedAtEpoch.eq('0')
         } catch (err) {
           this.logger.warn(
