8be38cbf5f423fcb499e38e381cab8041be29c05
Ford <ford@thegraph.com>
2022-01-17 14:51:49 -0800

    indexer-agent: Update DB migrations
    - Upgrade umzug dependency.
    - Setup umzug migrations CLI.
    - Move migrations to db directory.

---
+++ packages/indexer-agent/src/db/cli/umzug.ts
@@ -0,0 +1,78 @@
+import path from 'path'
+import { Umzug, SequelizeStorage } from 'umzug'
+import { Sequelize } from 'sequelize'
+import { createLogger } from '@graphprotocol/common-ts'
+
+const verbose_logging = process.env.VERBOSE === 'true'
+const log_level = verbose_logging ? 'trace' : 'info'
+const host = process.env.POSTGRES_HOST || 'localhost'
+const port = parseInt(process.env.POSTGRES_PORT || '5432')
+const database = process.env.POSTGRES_DATABASE
+const username = process.env.POSTGRES_USERNAME
+const password = process.env.POSTGRES_PASSWORD
+
+const logger = createLogger({
+  name: 'Migrations',
+  async: false,
+  level: log_level,
+})
+
+if (!database) {
+  throw Error(
+    `Database name not defined, please set 'POSTGRES_DATABASE' ENV var`,
+  )
+}
+if (!username) {
+  throw Error(
+    `Database 'username' name not defined, please set 'POSTGRES_USERNAME' ENV var`,
+  )
+}
+
+logger.debug('Connect to database', {
+  host,
+  port,
+  database,
+  username,
+  verbose_logging,
+})
+
+const sequelize = new Sequelize({
+  dialect: 'postgres',
+  host,
+  port,
+  username,
+  password,
+  database,
+  pool: {
+    max: 10,
+    min: 0,
+  },
+  logging: false,
+})
+
+logger.debug('Successfully connected to DB', { name: database })
+
+export const migrator = new Umzug({
+  migrations: {
+    glob: path.join(
+      __dirname,
+      '..',
+      '..',
+      '..',
+      'dist',
+      'db',
+      'migrations',
+      '*.js',
+    ),
+  },
+  context: {
+    queryInterface: sequelize.getQueryInterface(),
+    logger,
+  },
+  storage: new SequelizeStorage({
+    sequelize,
+  }),
+  logger: console,
+})
+
+export type Migration = typeof migrator._types.migration
---
+++ packages/indexer-agent/src/db/migrations/04-rename-fee-columns.ts
@@ -0,0 +1,127 @@
+import { Logger } from '@graphprotocol/common-ts'
+import { QueryInterface } from 'sequelize'
+
+interface MigrationContext {
+  queryInterface: QueryInterface
+  logger: Logger
+}
+
+interface Context {
+  context: MigrationContext
+}
+
+export async function up({ context }: Context): Promise<void> {
+  const { queryInterface, logger } = context
+
+  logger.info(`Rename fee columns`)
+
+  return await queryInterface.sequelize.transaction({}, async transaction => {
+    const tables = await queryInterface.showAllTables()
+
+    if (tables.includes('allocation_receipts')) {
+      const table = await queryInterface.describeTable('allocation_receipts')
+      if(table.paymentAmount) {
+        await queryInterface.renameColumn(
+          'allocation_receipts',
+          'paymentAmount',
+          'fees',
+          {transaction},
+        )
+      } else if (table.fees){
+        logger.info(
+          `Fees column already exists in the allocation_receipts table, column rename not necessary`,
+        )
+      }
+    }
+
+    if (tables.includes('allocation_summaries')) {
+      const table = await queryInterface.describeTable('allocation_summaries')
+      if(table.queryFees) {
+        await queryInterface.renameColumn(
+          'allocation_summaries',
+          'queryFees',
+          'collectedFees',
+          {transaction},
+        )
+      } else if (table.collectedFees)
+        logger.info(
+          `collectedFees column already exists in the allocation_receipts table, column rename not necessary`,
+        )
+        return
+    }
+
+    if (tables.includes('transfer_receipts')) {
+      const table = await queryInterface.describeTable('transfer_receipts')
+      if (table.paymentAmount) {
+        await queryInterface.renameColumn(
+          'transfer_receipts',
+          'paymentAmount',
+          'fees',
+          {transaction},
+        )
+      } else if (table.fees) {
+        logger.info(
+          `fees column already exists in the transfer_receipts table, column rename not necessary`,
+        )
+      }
+    }
+  })
+}
+
+export async function down({ context }: Context): Promise<void> {
+  const { queryInterface, logger } = context
+
+  logger.info(`Revert renaming fee columns`)
+
+  return await queryInterface.sequelize.transaction({}, async transaction => {
+    const tables = await queryInterface.showAllTables()
+
+    if (tables.includes('allocation_receipts')) {
+      const table = await queryInterface.describeTable('allocation_receipts')
+      if(table.fees) {
+        await queryInterface.renameColumn(
+          'allocation_receipts',
+          'fees',
+          'paymentAmount',
+          {transaction},
+        )
+      } else if (table.paymentAmount){
+        logger.info(
+          `paymentAmount column already exists in the allocation_receipts table, column rename not necessary`,
+        )
+      }
+    }
+
+    if (tables.includes('allocation_summaries')) {
+      const table = await queryInterface.describeTable('allocation_summaries')
+      if(table.collectedFees) {
+        await queryInterface.renameColumn(
+          'allocation_summaries',
+          'collectedFees',
+          'queryFees',
+          {transaction},
+        )
+      } else if (table.queryFees)
+        logger.info(
+          `queryFees column already exists in the allocation_receipts table, column rename not necessary`,
+        )
+      return
+    }
+
+    if (tables.includes('transfer_receipts')) {
+      const table = await queryInterface.describeTable('transfer_receipts')
+      if (table.fees) {
+        await queryInterface.renameColumn(
+          'transfer_receipts',
+          'fees',
+          'paymentAmount',
+          {transaction},
+        )
+      } else if (table.paymentAmount) {
+        logger.info(
+          `paymentAmount column already exists in the transfer_receipts table, column rename not necessary`,
+        )
+      }
+    }
+  })
+}
