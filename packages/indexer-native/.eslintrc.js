8b6e52e837f87f07588bb9a0db34d44e8760b4f2
Ford <ford@thegraph.com>
2022-04-20 14:24:46 -0700

    *: Reduce build size, include formatting where missing

---
+++ packages/indexer-native/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  "parserOptions": {
+    "ecmaVersion": 2020
+  },
+  "env": {
+    "node": true,
+    "commonjs": true,
+    "es6": true,
+    "jest": true
+  },
+  extends: ['eslint:recommended', 'prettier']
+}
--- packages/indexer-native/jest.config.js
+++ packages/indexer-native/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     collectCoverage: true,
-    preset: 'ts-jest',
     testEnvironment: 'node',
     testPathIgnorePatterns: ['/node_modules/', '/dist/', '.yalc']
 }
