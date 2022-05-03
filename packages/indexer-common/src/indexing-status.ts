86c245d57f263c110b412478817ed746b04d1e3b
hopeyen <hopey1118@gmail.com>
2022-05-03 15:59:43 -0700

    indexer-common: ensure numeric block number

--- packages/indexer-common/src/indexing-status.ts
+++ packages/indexer-common/src/indexing-status.ts
@@ -140,7 +140,7 @@ export class IndexingStatusResolver {
               `,
               {
                 subgraph: deployment.ipfsHash,
-                blockNumber: block.number,
+                blockNumber: +block.number,
                 blockHash: block.hash,
                 indexer: indexerAddress,
               },
