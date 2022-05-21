adffc7424e313fdbd63f9f7bf894821325c3c23b
Alexis Asseman <alexis@semiotic.ai>
2022-05-21 14:15:33 -0700

    indexer-service: Log query details and execution times

--- packages/indexer-service/src/query-fees/index.ts
+++ packages/indexer-service/src/query-fees/index.ts
@@ -1,8 +1,13 @@
 import { Address } from '@graphprotocol/common-ts'
+import { BigNumber } from 'ethers'
 
 export * from './allocations'
 
 export interface ReceiptManager {
   // Saves the query fees and returns the allocation for signing
-  add(receiptData: string): Promise<Address>
+  add(receiptData: string): Promise<{
+    id: string
+    allocation: Address
+    fees: BigNumber
+  }>
 }
