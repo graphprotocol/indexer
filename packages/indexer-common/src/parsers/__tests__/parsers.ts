95e5689caa52fb1182b9f45caba2e16c0323b9f9
tilacog <tilacog@gmail.com>
2023-05-05 21:06:58 -0300

    common: fix bug on export order
    Also added some tests while debugging.

--- packages/indexer-common/src/parsers/__tests__/parsers.ts
+++ packages/indexer-common/src/parsers/__tests__/parsers.ts
@@ -1,8 +1,21 @@
-import { parseTaggedUrl, parseTaggedIpfsHash } from '../validators'
+import {
+  parseTaggedUrl,
+  parseTaggedIpfsHash,
+  validateNetworkIdentifier,
+} from '../validators'
 
 const testUrlString = 'https://example.com/path/to/resource'
 const testUrl = new URL(testUrlString)
 const testCid = 'QmRKs2ZfuwvmZA3QAWmCqrGUjV9pxtBUDP3wuc6iVGnjA2'
+
+describe('validateNetworkIdentifier tests', () => {
+  it('should parse valid network identifiers', () => {
+    expect(validateNetworkIdentifier('goerli')).toBe('eip155:5')
+    expect(validateNetworkIdentifier('mainnet')).toBe('eip155:1')
+    expect(validateNetworkIdentifier('eip155:1')).toBe('eip155:1')
+    expect(validateNetworkIdentifier('eip155:5')).toBe('eip155:5')
+  })
+})
 
 describe('parseTaggedUrl tests', () => {
   it('should parse a URL without network id', () => {
