1022d1fa086cd31bd7f91ba6f6aebb216e713a75
tilacog <tilacog@gmail.com>
2023-05-26 15:05:08 -0300

    agent: refactor migration

--- packages/indexer-agent/src/db/migrations/11-add-protocol-network-field.ts
+++ packages/indexer-agent/src/db/migrations/11-add-protocol-network-field.ts
@@ -1,5 +1,7 @@
 import { Logger } from '@graphprotocol/common-ts'
-import { DataTypes, QueryInterface } from 'sequelize'
+import { QueryTypes, DataTypes, QueryInterface } from 'sequelize'
+
+const MANUAL_CONSTRAINT_NAME_FRAGMENT = '_composite_manual'
 
 interface MigrationContext {
   queryInterface: QueryInterface
@@ -11,83 +13,55 @@ interface Context {
   context: MigrationContext
 }
 
-const IndexingRules = 'IndexingRules'
-const Actions = 'Actions'
-const POIDispute = 'POIDispute'
-const protocolNetwork = 'protocolNetwork'
-const IndexingRulesOldPrimaryKeyConstraint = 'IndexingRules_pkey' // Assuming this is the existing constraint name
-const IndexingRulesNewPrimaryKeyConstraint = 'IndexingRules_composite_pkey'
-const IndexingRulesOldPrimaryKeyColumn = 'identifier'
-const POIDisputeOldPrimaryKeyConstraint = 'POIDispute_pkey' // Assuming this is the existing constraint name
-const POIDisputeNewPrimaryKeyConstraint = 'POIDispute_composite_pkey'
-const POIDisputeOldPrimaryKeyColumn = 'allocationID'
+interface MigrationInput {
+  table: string
+  oldPrimaryKeyColumns: string[]
+  newColumn: string
+}
+
+interface MigrationTarget extends MigrationInput {
+  oldPrimaryKeyConstraint: string
+  newPrimaryKeyConstraint: string
+}
+
+type AddColumnTarget = Pick<MigrationTarget, 'table' | 'newColumn'>
+
+const defaults: Pick<MigrationTarget, 'newColumn'> = {
+  newColumn: 'protocolNetwork',
+}
+
+const actionsTarget: AddColumnTarget = {
+  table: 'Actions',
+  ...defaults,
+}
+const indexingRulesInput: MigrationInput = {
+  table: 'IndexingRules',
+  oldPrimaryKeyColumns: ['identifier'],
+  ...defaults,
+}
+
+const poiDisputesInput: MigrationInput = {
+  table: 'POIDispute',
+  oldPrimaryKeyColumns: ['allocationID'],
+  ...defaults,
+}
 
 export async function up({ context }: Context): Promise<void> {
   const { queryInterface, networkChainId, logger } = context
   const m = new Migration(queryInterface, logger)
 
-  // Add protocolNetwork columns
-  await m.addColumn(Actions, protocolNetwork)
-  await m.addColumn(IndexingRules, protocolNetwork)
-  await m.addColumn(POIDispute, protocolNetwork)
-
-  // Update and relax constraints
-  await m.removeConstraint(IndexingRules, IndexingRulesOldPrimaryKeyConstraint)
-  await m.removeConstraint(POIDispute, POIDisputeOldPrimaryKeyConstraint)
-
-  // Populate the `protocolNetwork` columns with the provided network ID
-  await m.updateTable(IndexingRules, protocolNetwork, networkChainId)
-  await m.updateTable(Actions, protocolNetwork, networkChainId)
-  await m.updateTable(POIDispute, protocolNetwork, networkChainId)
-
-  // Restore constraints
-  await m.restorePrimaryKeyConstraint(
-    IndexingRules,
-    IndexingRulesNewPrimaryKeyConstraint,
-    [IndexingRulesOldPrimaryKeyColumn, protocolNetwork],
-  )
-  await m.restorePrimaryKeyConstraint(
-    POIDispute,
-    POIDisputeNewPrimaryKeyConstraint,
-    [POIDisputeOldPrimaryKeyColumn, protocolNetwork],
-  )
-
-  // Alter the `protocolNetwork` columns to be NOT NULL
-  await m.alterColumn(IndexingRules, protocolNetwork)
-  await m.alterColumn(Actions, protocolNetwork)
-  await m.alterColumn(POIDispute, protocolNetwork)
+  await m.addPrimaryKeyMigration(indexingRulesInput, networkChainId)
+  await m.addPrimaryKeyMigration(poiDisputesInput, networkChainId)
+  await m.addColumnMigration(actionsTarget, networkChainId)
 }
 
 export async function down({ context }: Context): Promise<void> {
   const { queryInterface, logger } = context
   const m = new Migration(queryInterface, logger)
 
-  // Drop the new primary key constraint
-  await queryInterface.removeConstraint(
-    IndexingRules,
-    IndexingRulesNewPrimaryKeyConstraint,
-  )
-  await queryInterface.removeConstraint(
-    POIDispute,
-    POIDisputeNewPrimaryKeyConstraint,
-  )
-
-  // Drop the new columns
-  await m.dropColumn(IndexingRules, protocolNetwork)
-  await m.dropColumn(Actions, protocolNetwork)
-  await m.dropColumn(POIDispute, protocolNetwork)
-
-  // Restore the old primary key constraint
-  await queryInterface.addConstraint(IndexingRules, {
-    fields: [IndexingRulesOldPrimaryKeyColumn],
-    type: 'primary key',
-    name: IndexingRulesOldPrimaryKeyConstraint,
-  })
-  await queryInterface.addConstraint(IndexingRules, {
-    fields: [POIDisputeOldPrimaryKeyColumn],
-    type: 'primary key',
-    name: POIDisputeOldPrimaryKeyConstraint,
-  })
+  await m.removePrimaryKeyMigration(indexingRulesInput)
+  await m.removePrimaryKeyMigration(poiDisputesInput)
+  await m.dropColumn(actionsTarget)
 }
 
 // Helper migration class
@@ -99,76 +73,220 @@ class Migration {
     this.logger = logger
   }
 
-  async addColumn(tableName: string, columnName: string): Promise<void> {
-    this.logger.debug(`Checking if ${tableName} table exists`)
-    const tables = await this.queryInterface.showAllTables()
-    if (!tables.includes(tableName)) {
-      this.logger.info(
-        `${tableName} table does not exist, migration not necessary`,
-      )
+  // Main migration steps in the UP direction
+  async addPrimaryKeyMigration(
+    input: MigrationInput,
+    networkChainId: string,
+  ): Promise<void> {
+    const tableExists = await this.checkTableExists(input)
+    if (!tableExists) {
       return
     }
 
-    this.logger.debug(`Checking if ${tableName} table needs to be migrated`)
-    const table = await this.queryInterface.describeTable(tableName)
-    if (columnName in table) {
+    // Infer primary key constraint names
+    const target = await this.processMigrationInput(input)
+
+    // Add protocolNetwork columns
+    await this.addColumn(target)
+
+    // Loosen constraints
+    await this.removeConstraint(target)
+
+    // Populate the `protocolNetwork` columns with the provided network ID
+    await this.updateTable(target, networkChainId)
+
+    // Restore constraints
+    await this.restorePrimaryKeyConstraint(target)
+
+    // Alter the `protocolNetwork` columns to be NOT NULL
+    await this.alterColumn(target)
+  }
+
+  // Main migration steps in the DOWN direction
+  async removePrimaryKeyMigration(input: MigrationInput): Promise<void> {
+    // Infer primary key constraint names
+    const target = await this.processMigrationInputDown(input)
+
+    // Drop the new primary key constraint
+    await this.removeNewConstraint(target)
+
+    // Drop the new columns
+    await this.dropColumn(target)
+
+    // Restore the old primary key constraint
+    await this.restoreOldPrimaryKeyConstraint(target)
+  }
+
+  // Actions follow a different path because they don't use the new
+  // column as a primary key
+  async addColumnMigration(
+    input: AddColumnTarget,
+    networkChainId: string,
+  ): Promise<void> {
+    // Add protocolNetwork columns
+    await this.addColumn(input)
+
+    // Populate the `protocolNetwork` columns with the provided network ID
+    await this.updateTable(input, networkChainId)
+
+    // Alter the `protocolNetwork` columns to be NOT NULL
+    await this.alterColumn(input)
+  }
+
+  async checkTableExists(
+    target: Pick<MigrationTarget, 'table'>,
+  ): Promise<boolean> {
+    this.logger.debug(`Checking if ${target.table} table exists`)
+    const exists = await this.queryInterface.tableExists(target.table)
+    if (exists) {
       this.logger.info(
-        `'${columnName}' columns already exist, migration not necessary`,
+        `${target.table} table does not exist, migration not necessary`,
       )
-      return
+      return false
+    }
+    return true
+  }
+
+  // Only for the UP step
+  async processMigrationInput(input: MigrationInput): Promise<MigrationTarget> {
+    const oldPrimaryKeyConstraint = await this.getPrimaryKeyConstraintName(
+      input,
+    )
+    const newPrimaryKeyConstraint =
+      oldPrimaryKeyConstraint + MANUAL_CONSTRAINT_NAME_FRAGMENT
+    return {
+      ...input,
+      oldPrimaryKeyConstraint,
+      newPrimaryKeyConstraint,
+    }
+  }
+  // Only for the DOWN step
+  async processMigrationInputDown(
+    input: MigrationInput,
+  ): Promise<MigrationTarget> {
+    const currentPrimaryKeyConstraint = await this.getPrimaryKeyConstraintName(
+      input,
+    )
+    let previousPrimaryKeyConstraint
+    if (currentPrimaryKeyConstraint.endsWith(MANUAL_CONSTRAINT_NAME_FRAGMENT)) {
+      previousPrimaryKeyConstraint = currentPrimaryKeyConstraint.replace(
+        MANUAL_CONSTRAINT_NAME_FRAGMENT,
+        '',
+      )
+    } else {
+      previousPrimaryKeyConstraint = `{input.table}_pkey`
     }
 
-    this.logger.info(`Add '${columnName}' column to ${tableName} table`)
-    await this.queryInterface.addColumn(tableName, columnName, {
+    return {
+      ...input,
+      newPrimaryKeyConstraint: currentPrimaryKeyConstraint,
+      oldPrimaryKeyConstraint: previousPrimaryKeyConstraint,
+    }
+  }
+
+  async getPrimaryKeyConstraintName(target: MigrationInput): Promise<string> {
+    const result: null | { constraint?: string } =
+      await this.queryInterface.sequelize.query(
+        `
+SELECT
+      con.conname as constraint
+FROM
+    pg_catalog.pg_constraint con
+    INNER JOIN pg_catalog.pg_class rel ON rel.oid = con.conrelid
+    INNER JOIN pg_catalog.pg_namespace nsp ON nsp.oid = connamespace
+WHERE
+    nsp.nspname = 'public'
+    AND rel.relname = :table
+    AND con.contype = 'p';
+`,
+        {
+          type: QueryTypes.SELECT,
+          replacements: { table: target.table },
+          plain: true,
+          raw: true,
+        },
+      )
+
+    if (!result || !result.constraint) {
+      throw new Error(
+        `Failed to infer primary key constraint name for table '${target.table}'`,
+      )
+    }
+    return result.constraint
+  }
+
+  async addColumn(target: AddColumnTarget): Promise<void> {
+    this.logger.info(
+      `Add '${target.newColumn}' column to ${target.table} table`,
+    )
+    await this.queryInterface.addColumn(target.table, target.newColumn, {
       type: DataTypes.STRING,
       allowNull: true,
     })
   }
 
-  async dropColumn(tableName: string, columnName: string): Promise<void> {
+  async dropColumn(target: AddColumnTarget): Promise<void> {
     const tables = await this.queryInterface.showAllTables()
-    if (tables.includes(tableName)) {
-      this.logger.info(`Drop '${columnName}' column from ${tableName} table`)
-      await this.queryInterface.removeColumn(tableName, columnName)
+    if (tables.includes(target.table)) {
+      this.logger.info(
+        `Drop '${target.newColumn}' column from ${target.table} table`,
+      )
+      await this.queryInterface.removeColumn(target.table, target.newColumn)
     }
   }
 
-  async updateTable(tableName: string, columnName: string, value: string) {
-    const values = { [columnName]: value }
-    const where = { [columnName]: null }
+  async updateTable(target: AddColumnTarget, value: string) {
+    const values = { [target.newColumn]: value }
+    const where = { [target.newColumn]: null }
     this.logger.info(
-      `Set '${tableName}' table '${columnName}' column to '${value}'`,
+      `Set '${target.table}' table '${target.newColumn}' column to '${value}'`,
     )
-    await this.queryInterface.bulkUpdate(tableName, values, where)
+    await this.queryInterface.bulkUpdate(target.table, values, where)
   }
 
-  async alterColumn(tableName: string, columnName: string) {
+  async alterColumn(target: AddColumnTarget) {
     this.logger.info(
-      `Altering ${tableName} table ${columnName} to be non-nullable`,
+      `Altering ${target.table} table ${target.newColumn} to be non-nullable`,
     )
-    await this.queryInterface.changeColumn(tableName, columnName, {
+    await this.queryInterface.changeColumn(target.table, target.newColumn, {
       type: DataTypes.STRING,
       allowNull: true,
     })
   }
 
-  async removeConstraint(tableName: string, constraintName: string) {
+  async removeConstraint(target: MigrationTarget) {
     this.logger.info(
-      `Temporarily removing primary key constraints from ${tableName}`,
+      `Temporarily removing primary key constraints from ${target.table}`,
     )
-    await this.queryInterface.removeConstraint(tableName, constraintName)
+    await this.queryInterface.removeConstraint(
+      target.table,
+      target.oldPrimaryKeyConstraint,
+    )
   }
 
-  async restorePrimaryKeyConstraint(
-    tableName: string,
-    newConstraintName: string,
-    columns: string[],
-  ) {
-    this.logger.info(`Restoring primary key constraints from ${tableName}`)
-    await this.queryInterface.addConstraint(tableName, {
-      fields: columns,
+  async restorePrimaryKeyConstraint(target: MigrationTarget) {
+    this.logger.info(`Restoring primary key constraints from ${target.table}`)
+    await this.queryInterface.addConstraint(target.table, {
+      fields: [...target.oldPrimaryKeyColumns, target.newColumn],
       type: 'primary key',
-      name: newConstraintName,
+      name: target.newPrimaryKeyConstraint,
+    })
+  }
+
+  // Only for the DOWN step
+  async removeNewConstraint(target: MigrationTarget) {
+    await this.queryInterface.removeConstraint(
+      target.table,
+      target.newPrimaryKeyConstraint,
+    )
+  }
+
+  // Only for the DOWN step
+  async restoreOldPrimaryKeyConstraint(target: MigrationTarget) {
+    await this.queryInterface.addConstraint(target.table, {
+      fields: target.oldPrimaryKeyColumns,
+      type: 'primary key',
+      name: target.oldPrimaryKeyConstraint,
     })
   }
 }
