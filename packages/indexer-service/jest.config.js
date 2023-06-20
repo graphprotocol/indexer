a1ac1ab3fa0bbee4f935db4429d406b25db19301
tilacog <tilacog@gmail.com>
2023-06-20 11:42:20 -0300

    *: add forceExit flag to jest tests

--- packages/indexer-service/jest.config.js
+++ packages/indexer-service/jest.config.js
@@ -1,29 +1,30 @@
-const bail = (s) => {
+const bail = s => {
   throw new Error(s)
 }
 
 module.exports = {
   collectCoverage: true,
+  forceExit: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/', '/dist/', '.yalc'],
   globals: {
-    "ts-jest": {
+    'ts-jest': {
       isolatedModules: true,
     },
     __DATABASE__: {
       host: process.env.POSTGRES_TEST_HOST || bail('POSTGRES_TEST_HOST is not defined'),
       port: parseInt(process.env.POSTGRES_TEST_PORT || '5432'),
       username:
-          process.env.POSTGRES_TEST_USERNAME ||
-          bail('POSTGRES_TEST_USERNAME is not defined'),
+        process.env.POSTGRES_TEST_USERNAME ||
+        bail('POSTGRES_TEST_USERNAME is not defined'),
       password:
-          process.env.POSTGRES_TEST_PASSWORD ||
-          bail('POSTGRES_TEST_PASSWORD is not defined'),
+        process.env.POSTGRES_TEST_PASSWORD ||
+        bail('POSTGRES_TEST_PASSWORD is not defined'),
       database:
-          process.env.POSTGRES_TEST_DATABASE ||
-          bail('POSTGRES_TEST_DATABASE is not defined'),
+        process.env.POSTGRES_TEST_DATABASE ||
+        bail('POSTGRES_TEST_DATABASE is not defined'),
     },
-    __LOG_LEVEL__: process.env.LOG_LEVEL || 'info'
+    __LOG_LEVEL__: process.env.LOG_LEVEL || 'info',
   },
 }
