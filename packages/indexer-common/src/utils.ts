c8fc6dd8ec5c44b4654e3c5cf32c3553d3e13929
tilacog <tilacog@gmail.com>
2023-08-23 15:01:57 -0300

    *: use common-ts v2.0.3

--- packages/indexer-common/src/utils.ts
+++ packages/indexer-common/src/utils.ts
@@ -8,17 +8,6 @@ import { Logger, Metrics, timer } from '@graphprotocol/common-ts'
 import { indexerError, IndexerErrorCode } from './errors'
 import { Sequelize } from 'sequelize'
 
-interface ConnectOptions {
-  host: string
-  port?: number
-  username: string
-  password: string
-  database: string
-  logging?: (sql: string, timing?: number) => void
-  poolMin?: number
-  poolMax?: number
-}
-
 export const parseBoolean = (
   val: string | boolean | number | undefined | null,
 ): boolean => {
@@ -73,36 +62,4 @@ export async function monitorEthBalance(
       })
     }
   })
-}
-
-// Copied from @graphprotocol/common-ts, but adding pool size options
-// TODO: Add these options in common-ts and remove this
-export const connectDatabase = async (options: ConnectOptions): Promise<Sequelize> => {
-  const { host, username, password, database, logging } = options
-
-  // Use port 5432 by default
-  const port = options.port || 5432
-  const poolMin = options.poolMin || 0
-  const poolMax = options.poolMax || 10
-
-  // Connect to the database
-  const sequelize = new Sequelize({
-    dialect: 'postgres',
-    host,
-    port,
-    username,
-    password,
-    database,
-    pool: {
-      max: poolMax,
-      min: poolMin,
-    },
-    logging,
-  })
-
-  // Test the connection
-  await sequelize.authenticate()
-
-  // All good, return the connection
-  return sequelize
 }
