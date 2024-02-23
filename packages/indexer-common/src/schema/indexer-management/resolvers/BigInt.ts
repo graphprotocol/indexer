1be1651a82a0d9250cdf1431efd9b39e5c61cb54
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2024-02-23 11:10:53 -0500

    scalars

--- packages/indexer-common/src/schema/indexer-management/resolvers/BigInt.ts
+++ packages/indexer-common/src/schema/indexer-management/resolvers/BigInt.ts
@@ -1,14 +1,3 @@
-import { GraphQLScalarType } from 'graphql'
-export const BigInt = new GraphQLScalarType({
-  name: 'BigInt',
-  description: 'BigInt description',
-  serialize: (value) => {
-    /* Implement logic to turn the returned value from resolvers to a value that can be sent to clients */
-  },
-  parseValue: (value) => {
-    /* Implement logic to parse input that was sent to the server as variables */
-  },
-  parseLiteral: (ast) => {
-    /* Implement logic to parse input that was sent to the server as literal values (string, number, or boolean) */
-  },
-})
+import { BigIntResolver } from 'graphql-scalars'
+
+export const BigInt = BigIntResolver
