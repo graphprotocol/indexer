9ce47c13f0dfafe752d0445fbe6d4061db42a32b
Daniel Werner <dan@edgeandnode.com>
2024-05-08 14:52:34 -0700

    indexer-integration-tests: start integration test package

---
+++ packages/indexer-integration-tests/jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  collectCoverage: true,
+  forceExit: true,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '.yalc'],
+  roots: ['<rootDir>/tests'],
+  transform: {
+      '^.+\\.ts?$': 'ts-jest',
+  },
+  setupFilesAfterEnv: ['jest-extended'],
+  globals: {
+      'ts-jest': { 'tsconfig': 'tsconfig.json'},
+  },
+  globalSetup: '<rootDir>/src/global-setup.ts',
+  globalTeardown: '<rootDir>/src/global-teardown.ts',
+}
---
+++ packages/indexer-integration-tests/src/global-teardown.ts
@@ -0,0 +1,4 @@
+
+export default async function globalTeardown(){
+  console.log('global teardown')
+}
