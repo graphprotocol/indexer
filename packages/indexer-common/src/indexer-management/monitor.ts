af220ff8a5157c1869e8a1716d05e77a12034e7a
Daniel Werner <dan@edgeandnode.com>
2024-12-27 14:28:20 -0800

    common: limit indexer status query by subgraph ids, add time logging for allocations query

--- packages/indexer-common/src/indexer-management/monitor.ts
+++ packages/indexer-common/src/indexer-management/monitor.ts
@@ -132,6 +132,7 @@ export class NetworkMonitor {
   }
 
   async allocations(status: AllocationStatus): Promise<Allocation[]> {
+    const startTimeMs = Date.now()
     try {
       this.logger.debug(`Fetch ${status} allocations`)
       let dataRemaining = true
@@ -202,6 +203,9 @@ export class NetworkMonitor {
         )
       }
 
+      this.logger.debug(
+        `Finished fetching ${status} allocations in ${Date.now() - startTimeMs}ms`,
+      )
       return allocations
     } catch (error) {
       const err = indexerError(IndexerErrorCode.IE010, error)
