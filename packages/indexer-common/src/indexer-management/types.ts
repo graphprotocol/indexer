87ed7521b9f47deadb4df4ce9e8c591ad207b39e
Daniel Werner <dan@edgeandnode.com>
2025-04-29 11:30:05 -0700

    all: bump to version 0.23.7

--- packages/indexer-common/src/indexer-management/types.ts
+++ packages/indexer-common/src/indexer-management/types.ts
@@ -231,18 +231,11 @@ const caip2ByChainAlias: { [key: string]: string } = {
 }
 
 async function buildCaip2MappingsFromRegistry() {
-  const networks = registry.networks
-
-  for (const network of networks) {
-    if (!network.aliases) {
+  for (const network of registry.networks) {
+    const alias = network.id
+    caip2ByChainAlias[alias] = network.caip2Id
+    if (!network.caip2Id.startsWith('eip155')) {
       continue
-    }
-    for (const alias of network.aliases) {
-      caip2ByChainAlias[alias] = network.caip2Id
-      if (alias.endsWith('-mainnet')) {
-        const aliasWithoutSuffix = alias.replace('-mainnet', '')
-        caip2ByChainAlias[aliasWithoutSuffix] = network.caip2Id
-      }
     }
     const chainId = parseInt(network.caip2Id.split(':')[1])
     if (
