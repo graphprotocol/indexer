410ddc8420ec755564d80c85b99ff0c6fea1c070
Tomás Migone <tomas@edgeandnode.com>
2025-09-22 10:11:47 -0300

    fix: apply pre-register provision check only in horizon
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- packages/indexer-common/src/network.ts
+++ packages/indexer-common/src/network.ts
@@ -576,18 +576,6 @@ export class Network {
     )
   }
   async register(): Promise<void> {
-
-    // First ensure the indexer has a provision, can't register without one
-    const provision = await this.contracts.HorizonStaking.getProvision(
-      this.specification.indexerOptions.address,
-      this.contracts.SubgraphService.target,
-    )
-
-    if (provision.createdAt === 0n) {
-      this.logger.info('Indexer does not have a provision, skipping registration.')
-      return
-    }
-
     const geoHash = geohash.encode(
       +this.specification.indexerOptions.geoCoordinates[0],
       +this.specification.indexerOptions.geoCoordinates[1],
@@ -626,6 +614,17 @@ export class Network {
   }
 
   private async _register(logger: Logger, geoHash: string, url: string): Promise<void> {
+    // First ensure the indexer has a provision, can't register without one
+    const provision = await this.contracts.HorizonStaking.getProvision(
+      this.specification.indexerOptions.address,
+      this.contracts.SubgraphService.target,
+    )
+
+    if (provision.createdAt === 0n) {
+      this.logger.info('Indexer does not have a provision, skipping registration.')
+      return
+    }
+
     const paymentsDestination =
       this.specification.indexerOptions.paymentsDestination?.toString() ??
       '0x0000000000000000000000000000000000000000'
